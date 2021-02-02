d26329ba862b2fc99d2d1aa4cdcc803934197844
root <parulben.thakkar@neosofttech.com>
2021-02-02 19:41:20 +0530

    deleted folder app/code

--- app/code/Algolia/Storelocator/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Copyright © 2021 Neosoft Technologies. All rights reserved.
- */
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Algolia_Storelocator',
-    __DIR__
-);
